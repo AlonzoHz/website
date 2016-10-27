f22f38ef1a7cb4c68e57877cd6c657dcbd08efaa
Alonzo Hernandez <alonzo.hernandez@gatech.edu>
2016-10-27 18:10:58 -0400

    Added slideshow

---
+++ javascript/slideshow.js
@@ -0,0 +1,18 @@
+var slide = 0;
+var slides = document.getElementsByClassName("slide");
+$(slides[slide]).show();
+console.log(slides.length);
+
+$("#arrow-right").click(function() {
+    $(slides[slide]).fadeOut(100);
+    slide += 1;
+    if(slide == slides.length) {slide = 0}
+    $(slides[slide]).fadeIn(100);
+});
+
+$("#arrow-left").click(function() {
+    $(slides[slide]).fadeOut(100);
+    slide -= 1;
+    if(slide < 0) {slide = slides.length - 1}
+    $(slides[slide]).fadeIn(100);
+});
